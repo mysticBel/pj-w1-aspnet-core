90a704bbb00df849ff9d4355fdeb4e53b8c253e8
mysticBel <dnxbel@gmail.com>
2023-07-07 22:21:42 -0500

    Creando primer view index

---
+++ sesion01/Controllers/ProductoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sesion01.Controllers
+{
+    public class ProductoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
